6d541e5de20c2e888415c3235074775f5cc7e4ef
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 11:39:56 +0530

    uc3 feature added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,15 @@
 public class EmployeeWage{
 	public static void main(String[] args) {
-		int FULL_TIME = 1;
+		int FULL_TIME = 2;
+		int PART_TIME = 1;
 		int WAGE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double check = Math.floor(Math.random() * 10) % 2;
+		double check = Math.floor(Math.random() * 10) % 3;
 		if (check == FULL_TIME)
 			empHrs = 8;
+		else if(check == PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		empWage = empHrs * WAGE_PER_HOUR;
