d0450e152859e594f389b16f48fd348f705af2a4
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 11:33:38 +0530

    uc2 feature added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,15 @@
 public class EmployeeWage{
 	public static void main(String[] args) {
 		int FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double check = Math.floor(Math.random() * 10) % 2;
 		if (check == FULL_TIME)
-			System.out.println("Employee is present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is absent");
+			empHrs = 0;
+		empWage = empHrs * WAGE_PER_HOUR;
+			System.out.println("Employee Wage :"+empWage);
 	}
 }
