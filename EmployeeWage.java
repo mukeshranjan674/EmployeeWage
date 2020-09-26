74cdb69da70e8938bf22e3aaa857ce1fd6c7c240
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-09-26 12:10:08 +0530

    uc5 feature added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,13 +3,17 @@ public class EmployeeWage{
 	public static final int PART_TIME = 1;
 	public static final int FULL_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int WORKING_DAYS = 20;
 	
 	public static void main(String[] args) {
 		
 		int empHrs = 0;
 		int empWage = 0;
-		double check = Math.floor(Math.random() * 10) % 3;
-		switch ((int)check) {
+		int totalWage = 0;
+		
+		for (int day = 0; day < WORKING_DAYS; day++) {
+			double check = Math.floor(Math.random() * 10) % 3;
+			switch ((int)check) {
 			case PART_TIME:
 				empHrs = 4;
 				break;
@@ -20,7 +24,11 @@ public class EmployeeWage{
 				empHrs = 0;
 			
 		}
-		empWage = empHrs * WAGE_PER_HOUR;
-			System.out.println("Employee Wage :"+empWage);
+			empWage = empHrs * WAGE_PER_HOUR;
+			totalWage = totalWage + empWage;
+			System.out.println("Employee Wage : "+empWage);
+		}
+		
+			System.out.println("Total Employee Wage :"+totalWage);
 	}
 }
